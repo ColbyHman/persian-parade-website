4940aed086917e081cbba0cd3d3589387afd5e21
Colby Hillman <colby.hillman@gmail.com>
2025-05-17 15:08:07 -0400

    Adjusted slider sizing

--- src/components/HomeSlider.jsx
+++ src/components/HomeSlider.jsx
@@ -8,28 +8,44 @@ import { Navigation, Pagination, Autoplay } from 'swiper/modules';
 const HomeSlider = () => {
   // Dynamically import all images from the folder
   const images = import.meta.glob('../assets/home_slider/*.{png,jpg,jpeg,gif}', { eager: true });
-
-  // images is an object: { './assets/slider/img1.jpg': Module, ... }
   const imageUrls = Object.values(images).map((mod) => mod.default);
 
   return (
-    <div className='w-full h-[400px] md:h-[500px]'>
-        <Swiper
+    <div className='w-screen overflow-hidden bg-black'>
+      <Swiper
         modules={[Navigation, Pagination, Autoplay]}
         navigation
         pagination={{ clickable: true }}
         autoplay={{ delay: 10000 }}
         loop={imageUrls.length > 1}
-        spaceBetween={30}
-        slidesPerView={1}
+        spaceBetween={20}
+        slidesPerView={1} // default fallback
         slidesPerGroup={1}
-        >
+        breakpoints={{
+          320: {
+            slidesPerView: 1,
+          },
+          640: {
+            slidesPerView: 2,
+          },
+          1024: {
+            slidesPerView: 3,
+          },
+          1440: {
+            slidesPerView: 4,
+          },
+        }}
+      >
         {imageUrls.map((src, idx) => (
-            <SwiperSlide key={idx}>
-            <img src={src} alt={`slide-${idx}`} className="w-full h-full object-cover" />
-            </SwiperSlide>
+          <SwiperSlide key={idx} className="flex">
+            <img
+              src={src}
+              alt={`slide-${idx}`}
+              className="w-full h-[50vh] object-cover"
+            />
+          </SwiperSlide>
         ))}
-        </Swiper>
+      </Swiper>
     </div>
   );
 };
