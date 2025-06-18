fe76b2b839f5b43bb9f458fbf31331346e9ed315
Colby Hillman <colby.hillman@gmail.com>
2025-06-17 21:50:50 -0400

    Removed broken press links

--- src/data/pressArchive.js
+++ src/data/pressArchive.js
@@ -35,23 +35,11 @@ const pressArchiveData = {
       link: "http://www.nbcnewyork.com/on-air/as-seen-on/Persian-Parade-Preview_New-York-375634241.html"
     },
     {
-      title: "IranWire",
-      link: "http://en.iranwire.com/features/7190/"
-    },
-    {
-      title: "North Jersey News",
-      link: "http://www.northjersey.com/photo-galleries/photos-13th-annual-persian-parade-1.1546952"
-    },
-    {
       title: "NY 1",
       link: "https://www.ny1.com/nyc/all-boroughs/news/2016/04/18/persian-pride-shines-at-annual-murray-hill-parade"
     }
   ],
   "2015": [
-    {
-      title: "NY 1",
-      link: "https://www.ny1.com/nyc/all-boroughs/news/2015/04/20/persian-pride-fills-streets-of-midtown-at-annual-parade-"
-    },
     {
       title: "NBC 4 New York",
       link: "http://www.nbcnewyork.com/on-air/as-seen-on/PERSIAN-PARADE_New-York-300412181.html"
@@ -61,22 +49,8 @@ const pressArchiveData = {
       link: "https://www.europanewswire.com/new-york-persian-parade-2015/"
     },
     {
-      title: "NY1",
-      link: "http://www.ny1.com/nyc/all-boroughs/news/2015/04/20/persian-pride-fills-streets-of-midtown-at-annual-parade-.html"
-    },
-    {
       title: "Zoroastrians.net",
       link: "http://zoroastrians.net/2015/04/21/persian-day-parade-april-2015/"
-    },
-    {
-      title: "The Hazlet News",
-      link: "http://hazletnews.com/2015-persian-day-parade/"
-    }
-  ],
-  "2014": [
-    {
-      title: "NBC 4 New York",
-      link: "http://www.nbcnewyork.com/on-air/as-seen-on/Persian-Parade-2014_New-York-254990931.html"
     }
   ],
   "2013": [
